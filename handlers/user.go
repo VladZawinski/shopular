ff755402c50e94f3da9fd4f1c77b29868d474fc7
Thiha Kaung Sett <thihakaungsett@zig.live>
2022-12-13 21:04:03 +0630

    Updated Deploy sh

--- handlers/user.go
+++ handlers/user.go
@@ -1,7 +1,15 @@
 package handlers
 
-import "shopular/services"
+import (
+	"shopular/services"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type UserHandler struct {
 	service services.UserService
+}
+
+func (uh UserHandler) FindAllUser(c fiber.Ctx) {
+
 }
